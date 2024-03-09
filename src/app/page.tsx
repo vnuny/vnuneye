4b3778879d5e88ecb2db3e55ca6d4f85faefc6f3
Diaa <deaa6757@gmail.com>
2024-03-10 00:27:18 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,28 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+
 export default function Home() {
-  const [data, setData] = React.useState([]);
-  useEffect(() => {
-    async function search() {
-      try {
-        const response = await axios.get("/api/search");
-        console.log(response.data);
-        setData(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    search();
-  }, [])
-  return (
-    <main>
-      <div className="card">
-        
-      </div>
-    </main>
-  );
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        async function search() {
+            try {
+                const response = await axios.get("/api/search");
+                console.log(response.data);
+                setData(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        search();
+    }, []);
+
+    return (
+        <main>
+            <div className="card">
+                <h2>{JSON.stringify(data)}</h2>
+            </div>
+        </main>
+    );
 }
